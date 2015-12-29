f94a518e8fe1038dd2d138570746de0751e305b5
miziyi <553967865@qq.com>
2015-12-29 15:28:39 +0800

    hehe

---
+++ heheTest/heheTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  heheTest
+//
+//  Created by xixi on 15/12/29.
+//  Copyright © 2015年 xixi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
